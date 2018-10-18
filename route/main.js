26eb95c018b14342bb7f6a2028ae2df087da9bce
LimSeunghun <limsh@saenoon.co.kr>
2018-10-18 15:34:08 +0900

    file upload

---
+++ route/main.js
@@ -0,0 +1,23 @@
+const upload = require("../func/upload");
+const analyzer = requ("audio-analyser");
+
+module.exports = function(app, upload)
+{
+    app.get("/", function(req, res){
+        res.render('index.html');
+    });
+
+    app.post("/analyze", upload.single("music_file"), function(req, res, next){
+
+        var file = req.file;
+    
+        let fileinfo = {
+            name : file.originalname,
+            mimetype : file.mimetype,
+            title : req.body.title,
+            artist : req.body.artist,
+        };
+    
+       res.json(fileinfo);
+    });
+}
